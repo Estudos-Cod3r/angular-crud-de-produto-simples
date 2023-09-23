3770eea3fd2bb999d0854540d71f9b236a76b334
GarciaNJ <nicolas.jg.njg@gmail.com>
2023-09-23 15:42:24 -0300

    Update product-service.service.ts

--- frontend/src/app/components/product/product-service.service.ts
+++ frontend/src/app/components/product/product-service.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import {MatSnackBar} from '@angular/material/snack-bar'
+import { Product } from './product.model';
+import { Observable, EMPTY } from 'rxjs'
+import { map, catchError} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductServiceService {
+  baseUrl = 'http://localhost:3001/products';
 
   constructor(private snackBar:MatSnackBar, private http: HttpClient) { }
 
@@ -15,6 +19,50 @@ export class ProductServiceService {
       horizontalPosition:'right',
       verticalPosition:'top',
       panelClass: isError ? ['msg-error'] : ['msg-success']
-    })
+    });
   }
+
+  errorHandler(e:any):Observable<any>{
+    this.showMessage('Ocorreu um erro', true);
+    return EMPTY;
+  }
+
+  read():Observable<Product>{
+    return this.http.get<Product>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  create(product:Product):Observable<Product>{
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  delete(id:number):Observable<Product>{
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  readById(id:number):Observable<Product>{
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  update(product:Product):Observable<Product>{
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url,product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
 }
